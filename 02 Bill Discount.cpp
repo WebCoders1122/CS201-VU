2d4d01e9640880df3b643fbd2eff9f47e6f734a9
web coders <webcoders1122@gmail.com>
2024-09-25 05:01:08 +0500

    bill discount program added

---
+++ 02 Bill Discount.cpp
@@ -0,0 +1,22 @@
+ // program to give discounts on different rates
+
+#include<iostream>
+using namespace std;
+int main(){
+	float bill, discountedBill;
+	cout << "Please Enter your bill = ";
+	cin >> bill;
+	if (bill > 20000){
+		discountedBill = bill - (bill * 0.2);
+			cout << "You final bill after 20% discount is " << discountedBill << endl;
+	}else	if (bill > 15000 && bill < 20000){
+		discountedBill = bill - (bill * 0.15);
+			cout << "You final bill after 15% discount is " << discountedBill<< endl;
+	}else if (bill > 5000 && bill < 15000){
+		discountedBill = bill - (bill * 0.05);
+			cout << "You final bill after 5% discount is " << discountedBill<< endl;
+	}else{
+			cout << "You final bill after 0% discount is " << bill << endl;
+	}
+	return 0;
+}
