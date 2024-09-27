ffb5502f7bb731d925fa6b24f03ce53fb6d48237
web coders <webcoders1122@gmail.com>
2024-09-27 09:33:22 +0500

    10: final read and write program

--- 10 file handling fstream lib/10a writing charactor to file.cpp
+++ 10 file handling fstream lib/10a writing charactor to file.cpp
@@ -1,16 +1,39 @@
 #include<iostream>
+#include<string>
 #include<fstream>
 using namespace std;
-main(){
+int main(){
+//	veriable declaration
 	ofstream myfile;
-	string text;
-//	reading and saving file contents in to myfile veriable
-	myfile.open("file.txt");
-//	writing myfile veriable contents to text veriable
-	myfile.put('1');
-//	printing
-	cout << text;
-//	closing file
+	string hello;
+//	creating file name output.txt
+	myfile.open("output.txt");
+	if(!myfile){
+		cerr << "Error: Could not open the file for reading!" << endl;
+		return 1;
+	}
+//	getting user input
+cout << "Please type something to write in the file: ";
+getline(cin, hello);
+//	writing string to file
+	myfile << hello;
 	myfile.close();
 	
+//	reading file string
+	// veriables
+	ifstream myfile2("output.txt");
+	string hello2;
+	//error throwing
+	if(!myfile2){
+		cerr << "Error: Could not open the file for reading!" << endl;
+		return 1;
+	}
+	//reading file opening
+	getline(myfile2, hello2);
+	cout << "-" << hello2 <<"." << endl;
+	cout<<  "is success written to file.";
+	
+	myfile2.close();
+	
+	return 0;
 }
