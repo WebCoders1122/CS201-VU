dc7427625e47e798ef9ec2ef65a84134ca746ec2
web coders <webcoders1122@gmail.com>
2024-09-28 14:21:54 +0500

    17: scope variables example

---
+++ 17 assignment of variable and scope.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+// declration
+int x;
+for (x=0; x<5; x++) {
+	cout << "value of x is : " << x << endl;
+	int temp = 12;
+	cout << "value of temp is : " << temp << endl << endl;
+};
+cout << "\n value of x is : " << x << endl;
+	return 0;
+}
