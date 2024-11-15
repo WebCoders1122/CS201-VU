3955e51b59aba296f6d80ae841343f23fc5e4cee
Web Coders <webcoders1122@gmail.com>
2024-11-15 20:01:15 +0500

    practice.cpp added

---
+++ Practice programs/00 Practice.cpp
@@ -0,0 +1,15 @@
+
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main(){
+	char name[10] = {'i', 'm'};
+
+	cout << name << endl;
+	for(int i=0; i<10; i++){
+		cout << name[i] << endl;
+	}
+	return 0;
+}
+
