517a801f2da4c615a1e63338d7a6008f63e58ccc
Web Coders <webcoders1122@gmail.com>
2024-11-15 21:27:08 +0500

    lacture 13: 03 unit matrix program

--- Practice programs/Lacture # 13/01 transpose of a matrix.cpp
+++ Practice programs/Lacture # 13/01 transpose of a matrix.cpp
@@ -1,24 +1,24 @@
 #include<iostream>
 using namespace std;
 int main(){
-	int number[3][3], temp;	
+	int number[5][5], temp;	
 	cout << "Please enter matrix values 1 by 1: ";
 	//getting values from user
-	for(int row = 0; row < 3; row++){
-		for(int col = 0; col < 3; col++){
+	for(int row = 0; row < 5; row++){
+		for(int col = 0; col < 5; col++){
 			cin >> number[row][col];
 		}
 	}
 	//display before transpose
-		for(int row = 0; row < 3; row++){
-		for(int col = 0; col < 3; col++){
+		for(int row = 0; row < 5; row++){
+		for(int col = 0; col < 5; col++){
 			cout << "\t" << number[row][col];
 		}
 		cout << endl;
 	}
 	// transpose 
-	for(int row = 0; row < 3; row++){
-		for(int col = row; col < 3; col++){
+	for(int row = 0; row < 5; row++){
+		for(int col = row; col < 5; col++){
 			temp = number[row][col];
 			number[row][col] = number[col][row];
 			number[col][row] = temp;
@@ -30,8 +30,8 @@ int main(){
 				cout << endl;
 				cout << endl;
 				cout << endl;
-	for(int row = 0; row < 3; row++){
-		for(int col = 0; col < 3; col++){
+	for(int row = 0; row < 5; row++){
+		for(int col = 0; col < 5; col++){
 			cout << "\t" << number[row][col];
 		}
 		cout << endl;
---
+++ Practice programs/Lacture # 13/03 identity matrix.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+
+bool isUnitMatrix(int matrix[][100], int n){
+	for(int row = 0; row < n; row++){
+		for(int col = 0; col < n; col++){
+			if(row == col && matrix[row][col] != 1){
+				return false;
+			}else if(row != col && matrix[row][col] != 0) {
+				return false;
+			}
+		
+		}
+	}
+	return true;
+}
+int main(){
+	int n; //size of matrix
+	cout << "Please enter size of squar matrix: ";
+	cin >> n;
+	int matrix[100][100];
+	cout << "Please enter matrix values 1 by 1: ";
+	//getting values from user
+	for(int row = 0; row < n; row++){
+		for(int col = 0; col < n; col++){
+			cin >> matrix[row][col];
+		}
+	}
+	//checking for unit matrix
+	if(isUnitMatrix(matrix, n)){
+		cout << "\nthis is unit matrix";
+	}else{
+		cout << "\nthis is not unit matrix";
+	}
+
+	return 0;
+}
