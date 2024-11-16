862783f99174564277e2d6231216ee1551e57e70
Web Coders <webcoders1122@gmail.com>
2024-11-16 21:30:06 +0500

    lac 14: 03 string to flip case

---
+++ Practice programs/Lacture # 14/03 flip case.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<cctype>
+using namespace std;
+void toFlipCase(char *str){
+	while(*str != '\0'){
+		if(islower(*str)){
+			*str = toupper(*str);
+		}else if(isupper(*str)){
+			*str = tolower(*str);
+		}
+	str++;
+	}
+}
+int main(){
+	char str[40];
+	//taking user input
+	cout << "please input a string of 0-40 characters: ";
+	cin.getline(str, 40);
+	// converting to uppercase
+	toFlipCase(str);
+	//display uppercase string
+	cout << str;
+	return 0;
+}
